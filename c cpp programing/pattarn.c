73707156c2f7d3c98bd686baac4f57624564772c
AnwarKawsar <kawsar_anwar@yahoo.com>
2023-11-25 22:56:48 +0600

    //

--- c cpp programing/pattarn.c
+++ c cpp programing/pattarn.c
@@ -2,13 +2,14 @@
 int main()
 {
     int n=0;
-    printf("Enter a number:\n");
+    printf("Enter a number:");
     scanf("%d",&n);
-    
-    for(int i=1;i>=n;i++){
-        for(int p=i;p>=0;i--){
-            printf("%d",i);
+
+    for(int i=1;i<=n;i++){
+        for(int k=i;k>0;k--){
+            printf("* ");
         }
+        
         printf("\n");
     }
     return 0;
