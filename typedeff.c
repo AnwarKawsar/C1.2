a979fb29d494f8342b49809b2802795b2d789e88
AnwarKawsar <kawsar_anwar@yahoo.com>
2023-12-25 23:40:18 +0600

    //

--- typedeff.c
+++ typedeff.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 
 int main(){
-    typedef float cgpa;
-    \
-    
-    
-    printf("Your CGPA is %f\n",cgpai);
+    typedef struct typedeff flt;
+    {
+        int cp;
+    };
+     scanf("%d",flt.cp)
+    printf("Your CGPA is %f\n",flt.cp);
     return 0;
 }
